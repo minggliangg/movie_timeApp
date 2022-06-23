a4ca28beb87c3a48f2ca895b881b85245e885671
Ming Liang <khongmingliang@icloud.com>
2022-06-23 21:42:55 +0800

    Initial Commit

---
+++ Movie Time/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movie Time
+//
+//  Created by Ming Liang Khong on 23/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Movie Time/Movie_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_TimeApp.swift
+//  Movie Time
+//
+//  Created by Ming Liang Khong on 23/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Movie_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
